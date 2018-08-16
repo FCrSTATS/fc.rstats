8f30d47bc715bdb7dfe3b72c8bd06413f4204b54
FC_rSTATS <35891964+FCrSTATS@users.noreply.github.com>
2018-08-16 09:37:57 +0000

    Add files via upload

--- R/parse_tracking.R
+++ R/parse_tracking.R
@@ -11,185 +11,79 @@
 #' @export
 
 parse_tracking <- function(tracking.xml, metadata){
-
-          print("// This process generally takes 45 minutes to process... go and make a cup of tea.......")
-
-##//--- Define some utility functions -------------------------------------------------- //
-
-    Unpack.1.Frame.of.Time <- function(frame.to.process){
-          
-          Parsing.Counter <<- 1
-          temp.parts <- unlist(strsplit(as.character(frame.to.process), ":", fixed = FALSE, perl = FALSE, useBytes = FALSE))
-
-          people.temp <- Unpack.1.Frame.of.Players(temp.parts[2])
-          ball.temp <- Unpack.1.Frame.of.Ball(temp.parts[3])
-          
-          people.temp$Ball.Ownership <- ball.temp$Ball.Ownership
-          people.temp$Ball.InPlay <- ball.temp$Ball.InPlay
-          people.temp$Ball.Contact.Info1 <- ball.temp$Ball.Contact.Info1
-          people.temp$Ball.Contact.Info2 <- ball.temp$Ball.Contact.Info2
-          
-          frame.temp <- bind_rows(people.temp, ball.temp)
-          
-          frame.temp$frameID <- temp.parts[1]
-
-          if((Parsing.Counter/250)%%1==0){cat(".")}else{}
-          Parsing.Counter <<- Parsing.Counter + 1
-
-          return(frame.temp)
-      
-    }
-    
-    
-    
-    Unpack.1.Frame.of.Players <- function(df){
-      
-        unpack.catch <- unlist(strsplit(df, ";")) %>% 
-        split(1:length(.)) %>% 
-        purrr::map(Unpack.1.Player.of.Tracking) %>% 
-        dplyr::bind_rows() 
-    
-        return(unpack.catch)
-    }
-    
-    
-    Unpack.1.Player.of.Tracking <- function(r){
-        
-              parts <- unlist(strsplit(unlist(r),","))
-              
-              return(data.frame(team_HA = as.numeric(as.character(parts[1])), 
-                           TrackID = as.numeric(as.character(parts[2])), 
-                           JerseyNo = as.numeric(as.character(parts[3])), 
-                           x = as.numeric(as.character(parts[4])), 
-                           y = as.numeric(as.character(parts[5])), 
-                           speed = as.numeric(as.character(parts[6])), 
-                           z = 10))
-    }
-    
-    
-    Unpack.1.Frame.of.Ball <- function(df){
-    
-         ball.parts <- unlist(strsplit(unlist(df),","))
-         return(data.frame(team_HA = 10, 
-                            TrackID = 50, 
-                            JerseyNo = NA, 
-                            x = as.numeric(as.character(ball.parts[1])), 
-                            y = as.numeric(as.character(ball.parts[2])), 
-                            z = as.numeric(as.character(ball.parts[3])),
-                            speed = as.numeric(as.character(ball.parts[4])),
-                            Ball.Ownership = ifelse(gsub(";","",ball.parts[5])=="A",0,1),
-                            Ball.InPlay = ifelse(gsub(";","",ball.parts[6])=="Dead",0,1),
-                            Ball.Contact.Info1 = ifelse(length(ball.parts[7])>0,ball.parts[7],NA),
-                            Ball.Contact.Info2 = ifelse(length(ball.parts[8])>0,ball.parts[8],NA)))  
-    }
-    
-
-##//--- Read Tracking Data ------------------------------------------------------------- //
   
-    dat <- read.table(tracking.xml)
-    dat <- dat$V1
-    cat("// Dat file loaded....")
-    
-##//--- Prep Tracking Data ------------------------------------------------------------- //
-
-    ## calculate the starting frame 
-    starting.frame <- unlist(strsplit(as.character(dat[1]), ":", fixed = FALSE, perl = FALSE, useBytes = FALSE))[1]
-
-    ## Trim off pre-game, half-time and post-game frames
-    starting.index.p1 <- (metadata$period1Start - as.numeric(as.character(starting.frame))) - 1
-    ending.index.p1 <- (metadata$period1End - as.numeric(as.character(starting.frame))) + 1
-
-    starting.index.p2 <- (metadata$period2Start - as.numeric(as.character(starting.frame))) - 1
-    ending.index.p2 <- (metadata$period2End - as.numeric(as.character(starting.frame))) + 1
-
-    starting.index.p3 <- (metadata$period3Start - as.numeric(as.character(starting.frame))) - 1
-    ending.index.p3 <- (metadata$period3End - as.numeric(as.character(starting.frame))) + 1
-
-    starting.index.p4 <- (metadata$period4Start - as.numeric(as.character(starting.frame))) - 1
-    ending.index.p4 <- (metadata$period4End - as.numeric(as.character(starting.frame))) + 1
-
-    indexes.to.select <- if(metadata$period4Start > 0){
-      c(starting.index.p1:ending.index.p1, starting.index.p2:ending.index.p2, starting.index.p3:ending.index.p3, starting.index.p4:ending.index.p4)}else{
-      c(starting.index.p1:ending.index.p1, starting.index.p2:ending.index.p2)}
-
-    dat <- dat[indexes.to.select] ## new trimmed data before we spilt it and deal with it... saves a lot of time!
-    cat("// Dat file trimmed....")
-
-    
-##//--- Process Tracking Data --------------------------------------------------------- //
-
-    dat.chunks <- split(dat, ceiling(seq_along(dat)/(length(dat)/10)))
-    
-    cat("//Unpacking starting....")
-    
-    tDAT1 <-  dat.chunks$`1` %>% 
-    split(1:length(.)) %>% 
-    purrr::map(Unpack.1.Frame.of.Time) %>% 
-    dplyr::bind_rows()
-    cat("//Section 1 of 10 complete.... ")  
-    
-    tDAT2 <-  dat.chunks$`2` %>% 
-    split(1:length(.)) %>% 
-    purrr::map(Unpack.1.Frame.of.Time) %>% 
-    dplyr::bind_rows()
-    cat("//Section 2 of 10 complete.... ")  
-
-    tDAT3 <-  dat.chunks$`3` %>% 
-    split(1:length(.)) %>% 
-    purrr::map(Unpack.1.Frame.of.Time) %>% 
-    dplyr::bind_rows()
-    cat("//Section 3 of 10 complete.... ")  
-
-    tDAT4 <-  dat.chunks$`4` %>% 
-    split(1:length(.)) %>% 
-    purrr::map(Unpack.1.Frame.of.Time) %>% 
-    dplyr::bind_rows()
-    cat("//Section 4 of 10 complete.... ")  
-
-    tDAT5 <-  dat.chunks$`5` %>% 
-    split(1:length(.)) %>% 
-    purrr::map(Unpack.1.Frame.of.Time) %>% 
-    dplyr::bind_rows()
-    cat("//Section 5 of 10 complete.... ")  
+            dat <- read.table(tracking.xml, stringsAsFactors = F)
+            dat <- dat$V1
+            
+            metadata <- parse_CH_metadata(metaXML)
+            
+                ## calculate the starting frame 
+            starting.frame <- unlist(strsplit(as.character(dat[1]), ":", fixed = FALSE, perl = FALSE, useBytes = FALSE))[1]
         
-    tDAT6 <-  dat.chunks$`6` %>% 
-    split(1:length(.)) %>% 
-    purrr::map(Unpack.1.Frame.of.Time) %>% 
-    dplyr::bind_rows()    
-    cat("//Section 6 of 10 complete.... ")  
-    
-    tDAT7 <-  dat.chunks$`7` %>% 
-    split(1:length(.)) %>% 
-    purrr::map(Unpack.1.Frame.of.Time) %>% 
-    dplyr::bind_rows()
-    cat("//Section 7 of 10 complete.... ")  
-      
-    tDAT8 <-  dat.chunks$`8` %>% 
-    split(1:length(.)) %>% 
-    purrr::map(Unpack.1.Frame.of.Time) %>% 
-    dplyr::bind_rows()
-    cat("//Section 8 of 10 complete.... ")  
+            ## Trim off pre-game, half-time and post-game frames
+            starting.index.p1 <- (metadata$period1Start - as.numeric(as.character(starting.frame))) - 1
+            ending.index.p1 <- (metadata$period1End - as.numeric(as.character(starting.frame))) + 1
         
-    tDAT9 <-  dat.chunks$`9` %>% 
-    split(1:length(.)) %>% 
-    purrr::map(Unpack.1.Frame.of.Time) %>% 
-    dplyr::bind_rows()
-    cat("//Section 9 of 10 complete.... ")  
-    
-    tDAT10 <-  dat.chunks$`10` %>% 
-    split(1:length(.)) %>% 
-    purrr::map(Unpack.1.Frame.of.Time) %>% 
-    dplyr::bind_rows()    
-    Unpack.1.Frame.of.Time(dat[1])
-    cat("//Section 10 of 10 complete.... ")  
-    
-##//--- Combine and Return Results --------------------------------------------------------- //
+            starting.index.p2 <- (metadata$period2Start - as.numeric(as.character(starting.frame))) - 1
+            ending.index.p2 <- (metadata$period2End - as.numeric(as.character(starting.frame))) + 1
+        
+            starting.index.p3 <- (metadata$period3Start - as.numeric(as.character(starting.frame))) - 1
+            ending.index.p3 <- (metadata$period3End - as.numeric(as.character(starting.frame))) + 1
+        
+            starting.index.p4 <- (metadata$period4Start - as.numeric(as.character(starting.frame))) - 1
+            ending.index.p4 <- (metadata$period4End - as.numeric(as.character(starting.frame))) + 1
+        
+            indexes.to.select <- if(metadata$period4Start > 0){
+              c(starting.index.p1:ending.index.p1, starting.index.p2:ending.index.p2, starting.index.p3:ending.index.p3, starting.index.p4:ending.index.p4)}else{
+              c(starting.index.p1:ending.index.p1, starting.index.p2:ending.index.p2)}
+        
+            dat <- dat[indexes.to.select] ## new trimmed data before we spilt it and deal with it... saves a lot of time!
+            cat("// Dat file trimmed....")
+        
+        
+        newdat = dat %>%
+                    unlist()%>%
+                    read.table(text = .,sep = ':')%>%
+                    as.matrix()%>%
+                    gsub(';','\n',.)%>%
+                    data.frame(stringsAsFactors = FALSE)
+        
+        dat1 = newdat[-3]%>%
+                     group_by(V1)%>%
+                     mutate(V2=list(read.csv(text=V2,header = F,stringsAsFactors = F)))%>%
+                     unnest()
+        
+        dat2 = newdat[-2]%>%
+                 group_by(V1)%>%
+                 mutate(V3=list(read.csv(text=V3,header = F,stringsAsFactors = F)))%>%
+                 unnest()
+        
+        cat("// Dat file split....")
+        
+        dat1 <- as.data.frame(dat1, stringsAsFactors = F )
+        dat2 <- as.data.frame(dat2, stringsAsFactors = F )
+        
+        colnames(dat1) <- c("FrameID", "removeCol", "team_HA", "TrackID", "JerseyNo", "x", "y", "speed")
+        colnames(dat2) <- c("FrameID", "removeCol", "x", "y", "z", "speed", "Ball.Ownership", "Ball.InPlay", "Ball.Contact.Info1")
+        
+        dat1$z <- 20
+        dat2$team_HA <- 10
+        dat2$TrackID <- 50
+        dat2$JerseyNo <- NA
+        
+        dat1 <- merge(dat1, dat2[c("FrameID", "Ball.Ownership", "Ball.InPlay", "Ball.Contact.Info1")], by= "FrameID")
+        
+        dat1$TrackID <- as.numeric(as.character(dat1$TrackID))
+        dat2$TrackID <- as.numeric(as.character(dat2$TrackID))
+        
+        dat.full <- bind_rows(dat1, dat2)
+        
+        dat.full <- within(dat.full, rm(removeCol))
+        
+        cat("// Tracking data merged....||")
 
-    tDAT <- bind_rows(list(tDAT1, tDAT2, tDAT3, tDAT4, tDAT5, tDAT6, tDAT7, tDAT8, tDAT9, tDAT10))
-    cat(paste0("//Tracking data parse complete with ", nrow(tDAT), " observations parse"))
+        return(dat.full)
+        
+} 
 
-    return(tDAT)
-    
-} # end fo parse_tracking function 
-    
-    
+
