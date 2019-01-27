331d5d19b8279c7d7f9946ae9e1309f57dbdfe21
FC_rSTATS <35891964+FCrSTATS@users.noreply.github.com>
2019-01-27 16:05:20 +0100

    minor change

--- R/parse_f7.R
+++ R/parse_f7.R
@@ -47,10 +47,10 @@ parse_f7 <- function(f7.xml, MatchID){
   lineup.catch$PlayerRef <- gsub("p","",lineup.catch$PlayerRef) ## remove P from the player ID
   colnames(lineup.catch)[2] <- "player_id"
   
-  team_break.finder = ifelse( lag(lineup.catch$Formation_Place, 1) == 0 & lineup.catch$Formation_Place != 0, 1, 0 )
-  lineup.catch$team_HA = unlist(lapply(team_break.finder,function(x) replace(x,is.na(x),0)))
-  lineup.catch$team_HA = cumsum(lineup.catch$team_HA)
-  lineup.catch$team_HA = abs(lineup.catch$team_HA - 1)
+  team_break_finder = ifelse( lag(lineup.catch$Formation_Place, 1) == 0 & lineup.catch$Formation_Place != 0, 1, 0 )
+  lineup_catch$team_HA = unlist(lapply(team_break_finder,function(x) replace(x,is.na(x),0)))
+  lineup_catch$team_HA = cumsum(lineup_catch$team_HA)
+  lineup_catch$team_HA = abs(lineup_catch$team_HA - 1)
   
   FirstName <- xpathSApply(pbpParse, paste("//", "Team/Player/PersonName/First", sep=""), xmlValue)
   LastName <- xpathSApply(pbpParse, paste("//", "Team/Player/PersonName/Last", sep=""), xmlValue)
